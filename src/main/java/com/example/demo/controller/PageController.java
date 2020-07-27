5994434eee03fe4c5426e21c1f25e2866b1caf36
18562919@qq.com <185629019@qq.com>
2020-07-27 20:35:46 +0800

    new git

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/ok")
+	@ResponseBody
+	public String getPage() {
+		return "it's very ok";
+	}
+}
